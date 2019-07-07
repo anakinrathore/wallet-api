04421b70c20b8286e4cc0abbfaee30a71d47409b
anakinrathore <cool.rathore55@gmail.com>
2019-07-07 13:05:39 +0530

    fix: Fix the wallet creation fucntionality and routes

--- spec/models/wallet_spec.rb
+++ spec/models/wallet_spec.rb
@@ -5,10 +5,12 @@ RSpec.describe Wallet, type: :model do
   it { should validate_presence_of(:balance_in_paise) }
 
   describe 'create' do
-    it 'should create a new wallet' do
+    it 'should create a new wallet for the user' do
+      user = create(:user)
       wallet = create(:wallet, balance_in_paise: 95000)
+      user.wallet = wallet
       expect(Wallet.count).to eq 1
-      expect(Wallet.first.balance_in_paise).to eq 95000
+      expect(user.wallet.balance_in_paise).to eq 95000
     end
   end
 end
