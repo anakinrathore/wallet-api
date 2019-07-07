3397325934199483ac26b8de944126a1c307165f
anakinrathore <cool.rathore55@gmail.com>
2019-07-07 16:11:40 +0530

    feat: Added spec and methods for wallet model for deposit and withdrawl of money

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,4 +1,16 @@
 class Wallet < ApplicationRecord
 
   validates_presence_of :balance_in_paise
+
+  def deposit(deposit_amount)
+    self.balance_in_paise = self.balance_in_paise + deposit_amount if not deposit_amount.negative?
+    self.save
+  end
+
+  def withdraw(withdrawl_amount)
+    if self.balance_in_paise > 0
+      self.balance_in_paise = self.balance_in_paise - withdrawl_amount if not withdrawl_amount.negative?
+      self.save
+    end
+  end
 end
--- spec/models/wallet_spec.rb
+++ spec/models/wallet_spec.rb
@@ -13,4 +13,49 @@ RSpec.describe Wallet, type: :model do
       expect(user.wallet.balance_in_paise).to eq 95000
     end
   end
+
+  describe 'deposit' do
+    it 'should deposit the wallet with a positive amount' do
+      user = create(:user)
+      wallet = create(:wallet, balance_in_paise: 95000)
+      user.wallet = wallet
+      wallet.deposit(5000)
+      expect(user.wallet.balance_in_paise).to eq 100000
+    end
+
+    it 'should not deposit the wallet with a negative amount' do
+      user = create(:user)
+      wallet = create(:wallet, balance_in_paise: 95000)
+      user.wallet = wallet
+      wallet.deposit(-400)
+      expect(user.wallet.balance_in_paise).to eq 95000
+    end
+  end
+
+  describe 'withdraw' do
+    it 'should withdraw the wallet with a positive amount' do
+      user = create(:user)
+      wallet = create(:wallet, balance_in_paise: 95000)
+      user.wallet = wallet
+      wallet.withdraw(5000)
+      expect(user.wallet.balance_in_paise).to eq 90000
+    end
+
+    it 'should not withdraw the wallet with a negative amount' do
+      user = create(:user)
+      wallet = create(:wallet, balance_in_paise: 95000)
+      user.wallet = wallet
+      wallet.withdraw(-400)
+      expect(user.wallet.balance_in_paise).to eq 95000
+    end
+
+    it 'should not withdraw the wallet when balance is 0 paise' do
+      user = create(:user)
+      wallet = create(:wallet, balance_in_paise: 0)
+      user.wallet = wallet
+      wallet.withdraw(500)
+      expect(user.wallet.balance_in_paise).to eq 0
+    end
+  end
+
 end
