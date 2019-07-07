7a3853509e9b7dfcf42e82244c4014aa9c8a7fd9
anakinrathore <cool.rathore55@gmail.com>
2019-07-07 14:59:26 +0530

    feat: Added tests for wallets controller for wallet creation

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,2 +1,12 @@
 class WalletsController < ApplicationController
+
+  def create
+    @user = User.find_by_id(params[:user_id])
+    if @user
+      @user.wallet = Wallet.create(balance_in_paise: params[:balance_in_paise])
+      render json: @user.wallet, status: :created
+    else
+      render json:{ "success": false, "errors": "User with id: #{params[:user_id]} not found" }, status: :bad_request
+    end
+  end
 end
--- spec/controllers/wallets_controller_spec.rb
+++ spec/controllers/wallets_controller_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe WalletsController, type: :controller do
 
+  describe 'POST#create' do
+    it 'should create a new wallet for the user' do
+      user = create(:user)
+      expect {
+        post :create, params: {  "user_id": user.id, "balance_in_paise": 50000 }
+      }.to change(Wallet, :count).by(1)
+
+    end
+
+    it 'should not create a new wallet if the user doesn\'t exist' do
+      expect {
+        post :create, params: {  "user_id": "1a4d0db0-7a86-4345-8980-0e7045032111", "balance_in_paise": 60000 }
+      }.to change(Wallet, :count).by(0)
+      expect(response).to have_http_status 400
+      expect(json["errors"]).to eq "User with id: 1a4d0db0-7a86-4345-8980-0e7045032111 not found"
+    end
+  end
 end
