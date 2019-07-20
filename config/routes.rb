f5720c03c2b6fff75eb1d8e36558b081cb7c7862
anakinrathore <cool.rathore55@gmail.com>
2019-07-20 16:56:57 +0530

    fix: Removing unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: :create
-  resources :wallets do
-    put 'deposit' => 'wallets#deposit', as: :deposit
-    put 'deposit_shared' => 'wallets#deposit_shared', as: :deposit_shared
-    put 'withdraw' => 'wallets#withdraw', as: :withdraw
-    put 'withdraw_shared' => 'wallets#withdraw_shared', as: :withdraw_shared
+  resources :wallets, only: [:create] do
+    put 'deposit' => 'wallets#deposit', on: :collection, as: :deposit
+    put 'deposit_shared' => 'wallets#deposit_shared', on: :collection, as: :deposit_shared
+    put 'withdraw' => 'wallets#withdraw', on: :collection, as: :withdraw
+    put 'withdraw_shared' => 'wallets#withdraw_shared', on: :collection, as: :withdraw_shared
     post 'create_shared' => 'wallets#create_shared',on: :collection, as: :create_shared
   end
   resources :groups, only: :create
